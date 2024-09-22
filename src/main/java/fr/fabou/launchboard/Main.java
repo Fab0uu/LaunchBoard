a10365022ae5eca5e10ce64b066051ee855fc911
921003 <fabricelox@gmail.com>
2024-09-22 06:09:25 +0200

    22/09

--- src/main/java/fr/fabou/launchboard/Main.java
+++ src/main/java/fr/fabou/launchboard/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         displayer.display();
         initMidi init = new initMidi();
-        //init.init();
+        //init.init();a
     }
 }
